2c4575c77f65ac08068d7168176e544c39d37cbc
Aishwarye Omer <aishwaryeomer@gmail.com>
2016-03-23 00:01:29 +0530

    Proposal commit

---
+++ helpers/serv.js
@@ -0,0 +1,53 @@
+
+//derived from original
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var scripts = [
+  'polyfills',
+  'confirm',
+  'method',
+  'liteajax',
+  'csrf'
+].map(function (s) { return ['<script src="/assets/javascripts/vanilla-ujs/', s, '.js"></script>'].join(''); });
+
+var template = function(body) {
+  return [
+    '<html><head><title>Testing…</title></head><body>',
+    body,
+    '</body></html>'
+  ].join("\n");
+};
+
+app.get('/fixture', function (req, res) {
+  res.send(template(scripts.join('')));
+});
+
+app.all('/echo', function (req, res) {
+  res.send(template([
+    '<script>window.top.',
+    req.query.callback,
+    '(',
+    JSON.stringify({
+      method: (req.body._method || req.method).toLowerCase(),
+      csrf: req.get('X-CSRF-Token'),
+      path: req.path
+    }),
+    ');</script>'
+  ].join('')));
+});
+
+app.all('/xhr', function (req, res) {
+  res.send({
+    method: (req.body._method || req.method).toLowerCase(),
+    csrf: req.get('X-CSRF-Token') || req.body[req.params['param']],
+    path: req.path
+  });
+});
+
+module.exports = app;
